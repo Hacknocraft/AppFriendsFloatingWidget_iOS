a4d09a2075bce5fcb85c2de8e062dff471b43355
wanghao <hao.wang@hacknocraft.com>
2016-12-13 02:00:50 -0500

    Initial Commit

---
+++ AppFriendsFloatingWidget/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppFriendsFloatingWidget
+//
+//  Created by HAO WANG on 12/13/16.
+//  Copyright © 2016 Hacknocraft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
